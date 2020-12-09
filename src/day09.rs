5cc38642028d24590082d655b051aaf42a4bcac2
Gisle Aune <dev@gisle.me>
2020-12-09 16:17:00 +0100

    add day09 alt part2.

--- src/day09.rs
+++ src/day09.rs
@@ -8,13 +8,16 @@ fn main() {
     let (list, dur_parse) = run_many(1000, || parse_input(&input));
     let (res_part1, dur_part1) = run_many(1000, || part1(&list, 25));
     let (res_part2, dur_part2) = run_many(100000, || part2(&list, res_part1));
+    let (res_part2_alt, dur_part2_alt) = run_many(100000, || part2_alt(&list, res_part1));
 
     print_result("P1", res_part1);
     print_result("P2", res_part2);
+    print_result("P2 ALT", res_part2_alt);
 
     print_time("Parse", dur_parse);
     print_time("P1", dur_part1);
     print_time("P2", dur_part2);
+    print_time("P2 ALT", dur_part2_alt);
     print_time("Total", dur_parse + dur_part1 + dur_part2);
 }
 
@@ -69,6 +72,81 @@ fn part2(data: &[u64], target: u64) -> u64 {
     smallest + largest
 }
 
+fn part2_alt(data: &[u64], target: u64) -> u64 {
+    let mut lower1 = (data.iter().position(|p| *p == target).unwrap() * 3) / 4;
+    let mut upper1 = lower1 + 1;
+    let mut lower2 = lower1;
+    let mut upper2 = upper1;
+    let mut sum1 = data[lower1] + data[upper1];
+    let mut sum2 = sum1;
+    let lower;
+    let upper;
+
+    loop {
+        if upper1 < data.len() {
+            if lower1 != upper1 {
+                let n = data[upper1 + 1];
+                if sum1 + n <= target {
+                    sum1 += n;
+                    upper1 += 1;
+                } else {
+                    sum1 -= data[lower1];
+                    lower1 += 1;
+                }
+                if sum1 == target {
+                    lower = lower1;
+                    upper = upper1;
+                    break;
+                }
+            } else {
+                lower1 += 1;
+                upper1 += 2;
+                if upper1 < data.len() {
+                    sum1 = data[lower1] + data[upper1];
+                }
+            }
+        }
+
+        if lower2 < data.len() {
+            if lower2 != upper2 {
+                let n = data[lower2 - 1];
+                if sum2 + n <= target {
+                    sum2 += n;
+                    lower2 -= 1;
+                } else {
+                    sum2 -= data[upper2];
+                    upper2 -= 1;
+                }
+                if sum2 == target {
+                    lower = lower2;
+                    upper = upper2;
+                    break;
+                }
+            } else {
+                upper2 -= 1;
+                lower2 -= 2;
+                if lower2 < data.len() {
+                    sum2 = data[lower2] + data[upper2];
+                }
+            }
+        }
+
+    }
+
+    let mut smallest = target;
+    let mut largest = 0;
+    for n in data[lower..=upper].iter().cloned() {
+        if n < smallest {
+            smallest = n;
+        }
+        if n > largest {
+            largest = n;
+        }
+    }
+
+    smallest + largest
+}
+
 const NEWLINE: u8 = '\n' as u8;
 const ZERO: u8 = '0' as u8;
 
@@ -104,5 +182,10 @@ mod tests {
     #[test]
     fn test_part2() {
         assert_eq!(part2(EXAMPLE, 127), 62);
+    }
+
+    #[test]
+    fn test_part2_alt() {
+        assert_eq!(part2_alt(EXAMPLE, 127), 62);
     }
 }
