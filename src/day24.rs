117d4218fdf33c5b7a769b366302f0f5e254c0f0
Gisle Aune <dev@gisle.me>
2020-12-24 13:27:00 +0100

    try day24 grid, but it's unfinished and not any faster.

--- src/day24.rs
+++ src/day24.rs
@@ -17,9 +17,9 @@ fn main() {
 
     print_time("Load", dur_load);
 
-    let (list, dur_parse) = run_many(1000, || parse_input(&input));
-    let ((res_part1, coords), dur_part1) = run_many(1000, || part1(&list));
-    let (res_part2, dur_part2) = run_many(1, || part2(&coords));
+    let ((list, max_len), dur_parse) = run_many(1000, || parse_input(&input));
+    let ((res_part1, coords), dur_part1) = run_many(1000, || part1_old(&list));
+    let (res_part2, dur_part2) = run_many(100, || part2(&coords, max_len));
 
     print_result("P1", res_part1);
     print_result("P2", res_part2);
@@ -30,7 +30,7 @@ fn main() {
     print_time("Total", dur_parse + dur_part1 + dur_part2);
 }
 
-fn part1(list: &[SmallVec<[Coordinate; 64]>]) -> (usize, Vec<Coordinate>) {
+fn part1_old(list: &[SmallVec<[Coordinate; 32]>]) -> (usize, Vec<Coordinate>) {
     let mut res = FxHashSet::default();
 
     for item in list.iter() {
@@ -46,78 +46,98 @@ fn part1(list: &[SmallVec<[Coordinate; 64]>]) -> (usize, Vec<Coordinate>) {
     (res.len(), res.into_iter().collect())
 }
 
-fn part2(coords: &[Coordinate]) -> usize {
-    let mut tile_states = FxHashMap::default();
-    let mut changes = Vec::with_capacity(128);
-    let mut new_whites = Vec::with_capacity(128);
-    for coord in coords.iter() {
-        tile_states.insert(*coord, true);
+fn part1(list: &[SmallVec<[Coordinate; 32]>], max_len: usize) -> (usize, Vec<Coordinate>) {
+    let width = max_len * 2;
+    let mut grid = vec![false; width * width];
+
+    for path in list.iter() {
+        let mut path_end = Coordinate(0, 0);
+        for offset in path.iter() {
+            path_end += *offset;
+        }
+
+        let index = path_end.to_index(width);
+        grid[index] = !grid[index];
     }
+
+    let res: Vec<Coordinate> = grid.iter()
+        .enumerate()
+        .filter(|(_, b)| **b)
+        .map(|(i, _)| Coordinate::from_index(i, width))
+        .collect();
+
+    (res.len(), res)
+}
+
+fn part2(coords: &[Coordinate], max_len: usize) -> usize {
+    let width = max_len * 2 + 202;
+    let mut grid = vec![false; width * width];
+    let mut changes = Vec::with_capacity(256);
+    for coord in coords.iter() {
+        grid[coord.to_index(width)] = true;
+    }
+    
+    let start = width + 1;
+    let end = grid.len() - start;
 
     for _ in 0..100 {
         changes.clear();
-        new_whites.clear();
 
         // Flip black tiles and find new whites.
-        for (coord, is_black) in tile_states.iter() {
+        for (index, is_black) in grid[start..end].iter().enumerate() {
+            let index = index + start;
+
+            let coord = Coordinate::from_index(index, width);
+
             if *is_black {
                 let mut count = 0;
                 for offset in NEIGHBOR_OFFSETS.iter() {
-                    let neigh = *coord + *offset;
-                    if let Some(other_is_black) = tile_states.get(&neigh) {
-                        if *other_is_black {
-                            count += 1;
+                    let neigh = coord + *offset;
+                    if grid[neigh.to_index(width)] {
+                        count += 1;
+                        if count > 2 {
+                            break;
                         }
-                    } else {
-                        new_whites.push(neigh);
                     }
                 }
 
                 if count != 1 && count != 2 {
-                    changes.push((*coord, false));
+                    changes.push((index, false));
                 }
-            }
-        }
-        for coord in new_whites.iter() {
-            tile_states.insert(*coord, false);
-        }
-
-        // Flip white tiles.
-        for (coord, is_black) in tile_states.iter() {
-            if !*is_black {
+            } else {
                 let mut count = 0;
                 for offset in NEIGHBOR_OFFSETS.iter() {
-                    let neigh = *coord + *offset;
-
-                    if let Some(other_is_black) = tile_states.get(&neigh) {
-                        if *other_is_black {
-                            count += 1;
-                            if count > 2 {
-                                break;
-                            }
+                    let neigh = coord + *offset;
+                    if grid[neigh.to_index(width)] {
+                        count += 1;
+                        if count > 2 {
+                            break;
                         }
                     }
                 }
 
                 if count == 2 {
-                    changes.push((*coord, true));
+                    changes.push((index, true));
                 }
             }
         }
 
-        for (coord, value) in changes.iter() {
-            *tile_states.get_mut(coord).unwrap() = *value;
+        for (index, value) in changes.iter() {
+            grid[*index] = *value;
         }
     }
 
-    tile_states.iter().filter(|(_, v)| **v).count()
+    grid.iter().filter(|v| **v).count()
 }
 
-fn parse_input(input: &str) -> Vec<SmallVec<[Coordinate; 64]>> {
-    input.lines().filter(|l| l.len() > 0).map(parse_line).collect()
+fn parse_input(input: &str) -> (Vec<SmallVec<[Coordinate; 32]>>, usize) {
+    let res: Vec<SmallVec<[Coordinate; 32]>> = input.lines().filter(|l| l.len() > 0).map(parse_line).collect();
+    let max_len = res.iter().map(|r| r.len()).max().unwrap();
+
+    (res, max_len)
 }
 
-fn parse_line(line: &str) -> SmallVec<[Coordinate; 64]> {
+fn parse_line(line: &str) -> SmallVec<[Coordinate; 32]> {
     let mut res = SmallVec::new();
 
     let mut modifier = 0u8;
@@ -148,8 +168,26 @@ fn parse_line(line: &str) -> SmallVec<[Coordinate; 64]> {
     res
 }
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 struct Coordinate(i32, i32);
+
+impl Coordinate {
+    fn to_index(&self, w: usize) -> usize {
+        let m = (w / 2) as i32;
+        let x = (self.0 + m) as usize;
+        let y = (self.1 + m) as usize;
+
+        (y * w) + x
+    }
+
+    fn from_index(index: usize, w: usize) -> Coordinate {
+        let x = (index % w) as i32;
+        let y = (index / w) as i32;
+        let m = (w / 2) as i32;
+
+        Coordinate(x - m, y - m)
+    }
+}
 
 impl Add for Coordinate {
     type Output = Coordinate;
@@ -169,11 +207,66 @@ impl AddAssign for Coordinate {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use term::Error::ColorOutOfRange;
+
+    const EXAMPLE_1: &str = "sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew";
 
     #[test]
     fn test_parse() {
         assert_eq!(parse_line("esenee").as_slice(), &[E, SE, NE, E]);
         assert_eq!(parse_line("esew").as_slice(), &[E, SE, W]);
         assert_eq!(parse_line("nwwswee").as_slice(), &[NW, W, SW, E, E]);
+    }
+
+    #[test]
+    fn test_old_part() {
+        let (input, max_len) = parse_input(EXAMPLE_1);
+
+        let (res_part1, mut coords) = part1(&input, max_len);
+        let (res_part1_old, mut coords_old) = part1_old(&input);
+
+        assert_eq!(res_part1, res_part1_old);
+
+        coords.sort();
+        coords_old.sort();
+        assert_eq!(coords, coords_old);
+    }
+
+    #[test]
+    fn test_index_convert() {
+        assert_eq!(Coordinate(-16, -16).to_index(32), 0);
+        assert_eq!(Coordinate(15, 15).to_index(32), (32*32)-1);
+        assert_eq!(Coordinate::from_index(0, 32), Coordinate(-16, -16));
+        assert_eq!(Coordinate::from_index((32*32)-1, 32), Coordinate(15, 15));
+
+        for w in 44..128 {
+            let m = (w / 2) as i32;
+
+            for x in -m..m {
+                for y in -m..m {
+                    let c = Coordinate(x, y);
+                    assert_eq!(Coordinate::from_index(c.to_index(w), w), c);
+                }
+            }
+        }
     }
 }
